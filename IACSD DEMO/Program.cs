59e1eba333bfdb14e4a2720a9ff61cab21bfd59a
kapil <kapil.bhudhia@klingelnberg.com>
2023-03-10 17:27:22 +0530

    Posting the entire solution

---
+++ IACSD DEMO/Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+using IACSD_DEMO;
+
+Console.WriteLine("Hello, World!");
+
+
+Heap heap = new Heap();
+List<int> elements = new List<int> { 23, 34, 45, -10, 15, 67, 99, 78, 100, 28, 100, -90, 89};
+
+int K = 5;
+
+foreach(int element in elements)
+{
+    if (heap.Count() < K)
+    {
+        heap.AddNewElement(element);
+
+        heap.PrintTheHeap();
+
+        Console.WriteLine();
+
+    }
+    else if (heap.Top() > element)
+    {
+        heap.ReplaceRootElement(element);
+        heap.PrintTheHeap();
+        Console.WriteLine();
+
+    }
+}
+
+
